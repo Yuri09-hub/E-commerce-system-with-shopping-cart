209c5ea3c68eb19759c568b36853d0f62dd2b215
Yuri Rodrigues <yurihenriques2004@gmail.com>
2025-12-29 19:53:42 +0100

    update

--- models.py
+++ models.py
@@ -1,9 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
-
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
 
 database = create_engine("sqlite:///database.db")
 
 base = declarative_base()
 
 
+#User
+class User(base):
+    __tablename__ = "users"
+
+    id = Column("ID User", Integer, primary_key=True, autoincrement=True)
+    name = Column("Name", String)
+    email = Column("Email", String, unique=True, nullable=False)
