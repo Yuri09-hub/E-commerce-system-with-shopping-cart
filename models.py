406f6aa9ff6c61da86ef0073b49e38c1e17afc69
Yuri Rodrigues <yurihenriques2004@gmail.com>
2025-12-29 20:38:50 +0100

    Update models.py

--- models.py
+++ models.py
@@ -7,10 +7,36 @@ database = create_engine("sqlite:///database.db")
 base = declarative_base()
 
 
+class Product(base):
+    __tablename__ = "products"
+
+    code_product = Column("Product_Code", Integer, primary_key=True)
+    name = Column("Product_Name", String, nullable=False)
+    description = Column("Description", String)
+    stock = Column("Stock", Integer, nullable=False)
+    category = Column("Category", String, nullable=False)
+
+
 #User
 class User(base):
     __tablename__ = "users"
 
-    id = Column("ID User", Integer, primary_key=True, autoincrement=True)
-    name = Column("Name", String)
+    id = Column("ID_User", Integer, primary_key=True, autoincrement=True)
+    name = Column("Name", String, nullable=False)
     email = Column("Email", String, unique=True, nullable=False)
+    password = Column("Password", String, nullable=False)
+    street = Column("Street", String, nullable=False)
+    city = Column("City", String, nullable=False)
+    province = Column("Province", String, nullable=False)
+    phone = Column("Phone", String)
+
+    def __init__(self, name, email, password, street, city, province, phone):
+        self.name = name
+        self.email = email
+        self.password = password
+        self.street = street
+        self.city = city
+        self.province = province
+        self.phone = phone
+
+class item_cart()
